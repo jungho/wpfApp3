98cc970e13a5b2cd3322874861cb405d93af5f2c
jungho <junghokim@microsoft.com>
2021-07-09 16:08:01 -0400

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowBrowser(object sender, RoutedEventArgs e)
+        {
+            Holder.Content = new EscherGroup.EssentialCP.Client.Internal.Controls.OmniBrowser(URL?.Text ?? "https://www.outlook.com");
+        }
+        private void HideBrowser(object sender, RoutedEventArgs e)
+        {
+            var browser = Holder.Content as EscherGroup.EssentialCP.Client.Internal.Controls.OmniBrowser;
+            if (browser != null)
+            {
+                browser.SetToBlank();                
+            }
+        }
+
+        private void ReplaceControl(object sender, RoutedEventArgs e)
+        {
+            Holder.Content = new TextBlock { Text = "Wait for browser" };
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
---
+++ OmniBrowser.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+
+
+namespace EscherGroup.EssentialCP.Client.Internal.Controls
+{
+    /// <summary>
+    /// Interaction logic for OmniBrowser.xaml
+    /// </summary>
+    public partial class OmniBrowser : UserControl
+    {
+        private System.Windows.Forms.WebBrowser _browser;
+
+        internal OmniBrowser( string url)
+        {
+            InitializeComponent();
+
+            _browser = new System.Windows.Forms.WebBrowser
+            {
+                MaximumSize = new System.Drawing.Size(7700, 7700),
+                IsWebBrowserContextMenuEnabled = false,
+                ScriptErrorsSuppressed = true,
+                WebBrowserShortcutsEnabled = false,
+                Url = new UriBuilder(url).Uri
+            };
+
+            FormsHost.Child = _browser;          
+            Unloaded += OmniBrowser_Unloaded;
+        }
+
+        internal void SetToBlank()
+        {
+            _browser?.Navigate("about:blank");            
+        }
+
+        private void OmniBrowser_Unloaded(object sender, RoutedEventArgs e)
+        {          
+             FormsHost.Dispose();
+            _browser?.Dispose();
+            _browser = null;
+        }     
+    }
+}
